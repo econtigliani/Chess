88396661cebf463a63a8e84fd877cad4340fe0fb
Emanuel Contigliani <emanuelconti@hotmail.com>
2020-11-22 20:06:49 -0300

    estructura básica

---
+++ responses/challenged.js
@@ -0,0 +1,10 @@
+function challenged(data){
+    return(JSON.stringify({
+        action: 'accept_challenge',
+        data: {
+            board_id : data.board_id
+        }
+    }))
+}
+
+module.exports.challenged = challenged;
