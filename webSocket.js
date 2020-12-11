3b4a02e622bb2318036c1a92e6cbc87dd57ca03f
Emanuel Contigliani <emanuelconti@hotmail.com>
2020-12-11 00:01:55 -0300

    Web socket params challenged

--- webSocket.js
+++ webSocket.js
@@ -1,14 +1,13 @@
-//importaciones
-var Challenged = require('./responses/challenged');
-var My_Turn = require('./responses/my_turn');
-var fs = require('fs');
+const Challenged = require('./responses/challenged');
+const My_Turn = require('./responses/my_turn');
+const fs = require('fs');
 const { client } = require('websocket');
 
 //lee mi authtoken de un archivo
-var authtoken = fs.readFileSync('authtoken.txt').toString();
+const authtoken = fs.readFileSync('authtoken.txt').toString();
 
 //generar cliente websocket.
-let ws = new client()
+const ws = new client()
 
 //genera el websocket
 function connect() {
@@ -38,13 +37,14 @@ ws.on('connect', function (connection) {
                 console.log("challenged by ", data.data.username)
 
                 //manda la respuesta al desafio
-                connection.sendUTF(Challenged.challenged(data.data));
+                connection.sendUTF(Challenged.challenged(data.data.board_id));
                 break;
 
             case 'your_turn':
 
                 //envio el movimiento que realizo
                 console.log(data.data);
+                //console.table(data.table)
                 connection.sendUTF(My_Turn.move(data.data));
                 break;
 
