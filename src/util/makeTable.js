a61af3e7fe02e7cbdd24c5d4e7fad9db97fa1c7a
Emanuel Contigliani <emanuelconti@hotmail.com>
2020-12-12 16:48:30 -0300

    FixGitErrorMakeTable

---
+++ src/util/makeTable.js
@@ -0,0 +1,21 @@
+const { max, min } = require("../enums/limitsBoard");
+
+//read board
+function maketable(board) {
+
+    let index = 0;
+    let matrix = [];
+
+    for (let i = min; i < max; i++) {
+        let row = [];
+        for (let j = min; j < max; j++) {
+            row.push(board[index]);
+            index++;
+        }
+        matrix.push(row)
+    }
+    return matrix;
+}
+module.exports = {
+    maketable : maketable,
+}
